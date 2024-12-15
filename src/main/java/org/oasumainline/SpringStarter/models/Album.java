f48f1135293d7b031bd0dc0ce58b1f94dfad417d
Orlando <orlando.sandi@idoagency.com>
2024-12-15 14:36:39 -0600

    fix: specify ID generation strategy

--- src/main/java/org/oasumainline/SpringStarter/models/Album.java
+++ src/main/java/org/oasumainline/SpringStarter/models/Album.java
@@ -14,7 +14,7 @@ import java.util.List;
 @EqualsAndHashCode
 public class Album {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String name;
--- src/main/java/org/oasumainline/SpringStarter/models/Artist.java
+++ src/main/java/org/oasumainline/SpringStarter/models/Artist.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Artist {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String name;
